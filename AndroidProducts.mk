c49122106eaa153a90537e61fb9b2e52e2f79faa
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-11-09 19:30:47 +0530

    oxygen: initial adaptation to derpfest AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oxygen.mk
+    $(LOCAL_DIR)/derp_oxygen.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_oxygen-user \
-    lineage_oxygen-userdebug \
-    lineage_oxygen-eng
+    derp_oxygen-user \
+    derp_oxygen-userdebug \
+    derp_oxygen-eng
