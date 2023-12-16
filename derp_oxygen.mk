a2150d50579b9e88e546b24dfdad6043c500cffd
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-12-17 00:21:58 +0530

    oxygen: disable lineage Trust HAL and Health HAL

--- derp_oxygen.mk
+++ derp_oxygen.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oxygen device.
 $(call inherit-product, device/xiaomi/oxygen/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
