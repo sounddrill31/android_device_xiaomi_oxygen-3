ba8789ad872673bab1f9987280e455b16e7c0d86
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-16 12:21:23 +0000

    Revert "oxygen: camera: Remove references of ion.h"
    This reverts commit 714a4894a46da7536de9a244df89d2dbe02fc35d.

--- camera/QCamera2/HAL3/QCamera3Mem.h
+++ camera/QCamera2/HAL3/QCamera3Mem.h
@@ -32,6 +32,10 @@
 
 // System dependencies
 #include <linux/msm_ion.h>
+#if TARGET_ION_ABI_VERSION >= 2
+#include <ion/ion.h>
+#include <linux/dma-buf.h>
+#endif //TARGET_ION_ABI_VERSION
 #include <utils/Mutex.h>
 
 // Camera dependencies
