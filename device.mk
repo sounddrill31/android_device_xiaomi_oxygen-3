ef47a3544d9478fbcbff688e9c9cda5ad5f6eaef
Han Sol Jin <hansol@hansol.ca>
2024-04-26 19:43:21 -0700

    oxygen: build common libqti-perfd-client stub
    Change-Id: I0af1d9cb5c2ad30fe9914f7fbb7f51778b49f003
    
    oxygen: build common libqti-perfd-client stub
    
    Change-Id: I0af1d9cb5c2ad30fe9914f7fbb7f51778b49f003

--- device.mk
+++ device.mk
@@ -37,7 +37,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     vendor/qcom/opensource/usb/etc \
-    hardware/lineage/interfaces/power-libperfmgr
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -304,7 +305,8 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
